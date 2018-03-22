55ef0b7d0ede3e53944c5f99f0b75576ae626d16
Laurent Almeras <laurent.almeras@kobalt-si.fr>
2018-03-22 15:59:42 +0100

    use clickable.sphinx

--- tasks.py
+++ tasks.py
@@ -1,265 +1,135 @@
-import invoke
-from invoke import task
-from invoke import Collection
+#! /usr/bin/env python
+from __future__ import print_function  # pylint: disable=unused-import
 
-import click
-import subprocess
-import getpass
+name = 'igloo-doc'
+default_command = 'click-igloodoc'
+entry_points = {
+    'console_scripts': [
+        'invoke-infra=invoke.main:program.run',
+        '{}=clickable.click:main'.format(default_command)
+    ]
+}
+bootstrap_sh = "https://raw.githubusercontent.com/lalmeras/bootstrap-virtualenv/master/bootstrap.sh"
+bootstrap_target = ".tools/bootstrap"
+bootstrap_requirements = "git+https://github.com/lalmeras/clickable.git@dev invoke"
 
+# use local version of bootstrap & clickable
+bootstrap_sh = "file:/home/lalmeras/git/bootstrap-virtualenv/bootstrap.sh"
+bootstrap_requirements = "git+file:/home/lalmeras/git/clickable/@dev invoke"
+bootstrap_requirements = "-efile:/home/lalmeras/git/clickable/ invoke"
+
+def bootstrap_done(bootstrapenv):
+    print("""
+**
+** activate environment with
+. {0}
+{1} --help
+**
+** Use {1} instead of ./task.py
+**""".format(bootstrapenv.activate, default_command))
+
+
+## Bootstrap clickable ##
+## do not modify       ##
+import contextlib, os, os.path, shutil, subprocess, sys, tempfile
 import os
-import re
+
+def prevent_loop(environ, env_name):
+    environ = dict(environ)
+    if env_name in environ:
+        print('** bootstrap loop detected; bootstrap environment installed but unable to load clickable. Aborting...')
+        sys.exit(1)
+    environ[env_name] = 'true'
+    return environ
+
+clean = '--clean' in sys.argv
+bootstrap_phase = 'CLICKABLE_BOOTSTRAP' in os.environ
+is_main = __name__ == '__main__'
+if is_main or bootstrap_phase:
+    # block for ./script.py invocation OR import during bootstrap_phase
+    if '--help' in sys.argv or '-h' in sys.argv:
+        # print usage
+        print("""Usage: {0} [--help|-h] [--clean]
+Install a virtualenv with minimal requirements ({1})""".format(__file__, bootstrap_requirements))
+        sys.exit(0)
+    if is_main and (clean or not os.path.exists(bootstrap_target)):
+        # download and run bootstrap.sh on missing target, or explicit clean
+        environ = prevent_loop(os.environ, 'BOOTSTRAP_INSTALL')
+        environ['BOOTSTRAP_TARGET'] = bootstrap_target
+        environ['BOOTSTRAP_REQUIREMENTS'] = bootstrap_requirements
+        try:
+            import urllib2 as urlreq # Python 2.x
+        except:
+            import urllib.request as urlreq # Python 3.x
+        req = urlreq.Request(bootstrap_sh)
+        (fd, script) = tempfile.mkstemp()
+        with open(script, 'w') as dest, \
+             contextlib.closing(urlreq.urlopen(req)) as src:
+            shutil.copyfileobj(src, dest)
+        subprocess.check_call(['bash', script], env=environ)
+    else:
+        try:
+            # either pip install or python setup.py handling
+            from clickable.bootstrap import run_setup
+            run_setup(__file__, name, entry_points=entry_points, callback=bootstrap_done)
+            raise Exception('unreachable code')
+        except ImportError:
+            pass
+    # rerun script with venv interpreter after install or import failure
+    command = list(sys.argv)
+    command.insert(0, os.path.join(bootstrap_target, 'bin/python'))
+    '--clean' not in command or command.remove('--clean') # clean must not be looped
+    environ = prevent_loop(os.environ, 'BOOTSTRAP_RERUN')
+    os.execve(command[0], command, environ)
+## Bootstrap clickable ##
 import json
+import logging
+import os
+import pprint
+import re
+import subprocess
+
 try:  # py3
     from shlex import quote
 except ImportError:  # py2
     from pipes import quote
 
-CONTEXT_SETTINGS = dict(
-    max_content_width=120
-)
+import invoke
+import click
+from ruamel.yaml import YAML
 
-@click.group(context_settings=CONTEXT_SETTINGS)
-def cli():
+import clickable.utils
+import clickable.sphinx
+import clickable.coloredlogs
+
+
+clickable.coloredlogs.bootstrap()
+logger = logging.getLogger('clickable')
+
+
+@click.group()
+@click.pass_context
+def click_igloodoc(ctx):
+    ctx.obj = {}
+    path_resolver = clickable.utils.PathResolver(sys.modules[__name__])
+    ctx.obj['path_resolver'] = path_resolver
+    ctx.obj['project_root'] = \
+        os.path.normpath(os.path.dirname(sys.modules[__name__].__file__))
+    conf_path = os.path.join(ctx.obj['project_root'], 'clickable.yaml')
+    if os.path.isfile(conf_path):
+        with open(conf_path) as f:
+            yaml = YAML(typ='safe')
+            configuration = yaml.load(f)
+            ctx.obj.update(configuration)
+    logger.info('loaded configuration: \n{}'.format(pprint.pformat(ctx.obj)))
     pass
 
 
-@task
-def _virtualenv(ctx, virtualenv_path=None):
-    """
-    Initialize a virtualenv folder.
-    """
-    virtualenv_path = virtualenv_path or ctx.virtualenv_path
-    if not check_virtualenv(ctx, virtualenv_path):
-        if not os.path.exists(os.path.dirname(virtualenv_path)):
-            os.makedirs(os.path.dirname(virtualenv_path))
-        ctx.run(' '.join(['virtualenv', virtualenv_path]))
-        if not check_virtualenv(ctx, virtualenv_path):
-            raise Exception('python install fails')
-
-
-@task
-def _sphinx(ctx, virtualenv_path=None, skip=None, version=None):
-    """
-    Install sphinx inside a virtualenv folder.
-    """
-    skip = skip if (skip is not None) \
-        else ctx.sphinx.skip
-    dependencies = ctx.sphinx.dependencies
-    if not skip:
-        virtualenv_path = virtualenv_path or ctx.virtualenv_path
-        package = ctx.sphinx.package_name
-        version = version or ctx.sphinx.version
-        _pip_package(ctx, package, version)
-        for dependency in dependencies:
-            _pip_package(ctx, dependency['name'],
-                         dependency.get('version', None))
-    else:
-        print('sphinx not managed (sphinx.skip: yes)')
-
-
-@task
-def _recommonmark(ctx, virtualenv_path=None, skip=None, version=None):
-    """
-    Install recommonmark inside a virtualenv folder.
-    """
-    skip = skip if (skip is not None) \
-        else ctx.recommonmark.skip
-    dependencies = ctx.dependencies
-    if not skip:
-        virtualenv_path = virtualenv_path or ctx.virtualenv_path
-        package = ctx.recommonmark.package_name
-        version = version or ctx.recommonmark.version
-        _pip_package(ctx, package, version)
-        for dependency in dependencies:
-            _pip_package(ctx, dependency['name'],
-                         dependency.get('version', None))
-    else:
-        print('recommonmark not managed (recommonmark.skip: yes)')
-
-@task
-def _sphinx_rtd_theme(ctx, virtualenv_path=None, skip=None, version=None):
-    """
-    Install recommonmark inside a virtualenv folder.
-    """
-    skip = skip if (skip is not None) \
-        else ctx.sphinx_rtd_theme.skip
-    dependencies = ctx.dependencies
-    if not skip:
-        virtualenv_path = virtualenv_path or ctx.virtualenv_path
-        package = ctx.sphinx_rtd_theme.package_name
-        version = version or ctx.sphinx_rtd_theme.version
-        _pip_package(ctx, package, version)
-        for dependency in dependencies:
-            _pip_package(ctx, dependency['name'],
-                         dependency.get('version', None))
-    else:
-        print('sphinx_rtd_theme not managed (sphinx_rtd_theme.skip: yes)')
-
-
-
-def _pip_package(ctx, package, version=None, virtualenv_path=None):
-    """
-    Install a pypi package (with pip) inside a virtualenv folder.
-    """
-    virtualenv_path = virtualenv_path or ctx.virtualenv_path
-    if not check_pip(ctx, virtualenv_path, package, version):
-        pip_install(ctx, virtualenv_path, package, version)
-        if not check_pip(ctx, virtualenv_path, package, version):
-            raise Exception('{} install fails'.format(package))
-
-
-@task(pre=[_virtualenv, _sphinx, _recommonmark, _sphinx_rtd_theme])
-def configure(ctx):
-    """
-    Trigger virtualenv and sphinx initialization.
-
-    All this tools are needed to handle documentation generation.
-    """
+@click_igloodoc.group()
+@click.pass_context
+def sphinx(ctx):
     pass
 
 
-def _docs_makefile(target, ctx, virtualenv_path=None):
-    """
-    Trigger a sphinx Makefile target. Used to delegate all documentation jobs to
-    original sphinx Makefile.
-    """
-    virtualenv_path = virtualenv_path or ctx.virtualenv_path
-    os.environ['PATH'] = \
-        ':'.join([
-            os.path.abspath(os.path.join(virtualenv_path, 'bin')),
-            os.environ['PATH']])
-    args = ['make', '-C', '.', target]
-    ctx.run(' '.join(args), pty=True)
-
-
-@task(pre=[configure])
-def docs(ctx, virtualenv_path=None):
-    """
-    Rebuild documentation.
-    """
-    _docs_makefile('html', ctx, virtualenv_path)
-
-
-@task(name='docs-clean', pre=[configure])
-def docs_clean(ctx, virtualenv_path=None):
-    """
-    Clean generated documentation.
-    """
-    _docs_makefile('clean', ctx, virtualenv_path)
-
-
-@task(name='docs-live', pre=[docs, configure])
-def docs_live(ctx, virtualenv_path=None):
-    """
-    Live build of documentation on each modification. Open a browser with a
-    local server to serve documentation. Opened page is reloaded each time
-    documentation is generated.
-    """
-    virtualenv_path = virtualenv_path or ctx.virtualenv_path
-    os.environ['PATH'] = \
-        ':'.join([
-            os.path.abspath(os.path.join(virtualenv_path, 'bin')),
-            os.environ['PATH']])
-    command = ' '.join([
-        'sphinx-autobuild',
-        '-B',
-        '--ignore', '"*.swp"',
-        '--ignore', '"*.log"',
-        '--ignore', '"*~"',
-        '--ignore', '"*~"',
-        '-b', 'html',
-        os.path.dirname(__file__) + '/source',
-        os.path.dirname(__file__) + '/build/html'
-    ])
-    ctx.run(command, pty=True)
-
-
-def check_virtualenv(ctx, virtualenv_path):
-    """
-    Check if virtualenv is initialized in virtualenv folder (based on
-    bin/python file).
-    """
-    r = ctx.run(' '.join([
-        os.path.join(virtualenv_path, 'bin/python'),
-        '--version'
-    ]), warn=True, hide='both')
-    return r.ok
-
-
-def check_pip(ctx, virtualenv_path, package, version):
-    """
-    Check if a pypi package is installed in virtualenv folder.
-    """
-    r = ctx.run(' '.join([
-        os.path.join(virtualenv_path, 'bin/pip'),
-        'show',
-        package
-    ]), hide='both', warn=True)
-    if not r.ok:
-        # pip show package error - package is not here
-        return False
-    if version is None:
-        # no version check needed
-        return True
-    # package here, check version
-    m = re.search(r'^Version: (.*)$', r.stdout, re.MULTILINE)
-    result = m is not None and m.group(1).strip() == version
-    return result
-
-
-def pip_install(ctx, virtualenv_path, package, version):
-    """
-    Install a pypi package in a virtualenv folder with pip.
-    """
-    pkgspec = None
-    if version is None:
-        pkgspec = package
-    else:
-        pkgspec = '{}=={}'.format(package, version)
-    ctx.run(' '.join([
-        os.path.join(virtualenv_path, 'bin/pip'),
-        'install',
-        pkgspec
-    ]))
-
-
-def _vcommand(virtualenv_path, command, *args):
-    """
-    Run a command from virtualenv folder.
-    """
-    cl = []
-    cl.append(os.path.join(virtualenv_path, 'bin', command))
-    cl.extend(args)
-    return ' '.join(cl)
-
-
-def _command(command, *args):
-    """
-    Run a command.
-    """
-    cl = []
-    cl.append(os.path.join(command))
-    cl.extend(args)
-    return ' '.join(cl)
-
-ns = Collection(configure,
-                docs, docs_live, docs_clean)
-ns.configure({
-    'sphinx': {
-        'package_name': 'sphinx',
-        'dependencies': [
-            { 'name': 'sphinx-bootstrap-theme' },
-            { 'name': 'sphinx-autobuild' }
-        ]
-    },
-    'recommonmark': {
-        'package_name': 'recommonmark'
-    },
-    'sphinx_rtd_theme': {
-        'package_name': 'sphinx_rtd_theme'
-    },
-    'dependencies': []
-})
-
-if __name__ == '__main__':
-    cli()
+sphinx_provider = lambda ctx: ctx.obj['sphinx']
+clickable.sphinx.sphinx_click_group(sphinx, sphinx_provider)
